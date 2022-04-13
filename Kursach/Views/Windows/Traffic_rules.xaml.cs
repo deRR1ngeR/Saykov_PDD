12cd21b7bca45e03f3d86195ba89465968369881
Cайко Андрей <Saykotechka76@mail.ru>
2022-04-13 03:58:00 +0300

    Fix last commit

--- Kursach/Views/Windows/Traffic_rules.xaml.cs
+++ Kursach/Views/Windows/Traffic_rules.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Kursach.Views
+namespace Kursach
 {
     /// <summary>
     /// Логика взаимодействия для Traffic_rules.xaml
