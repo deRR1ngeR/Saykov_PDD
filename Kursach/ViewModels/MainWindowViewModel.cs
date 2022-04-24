f3e00fa3f4e8b2705041840ec8d432f5a9c0dbef
Cайко Андрей <Saykotechka76@mail.ru>
2022-04-25 02:40:32 +0300

    Попытка добавления авторизации

--- Kursach/ViewModels/MainWindowViewModel.cs
+++ Kursach/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Kursach.Infrastructure.Commands;
+using Kursach.Models.Base;
+using Kursach.Models;
 using Kursach.ViewModels.Base;
 using System;
 using System.Collections.Generic;
@@ -6,29 +8,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows;
+using Kursach.Infrastructure.Commands.Base;
+
 
 namespace Kursach.ViewModels
 {
-    internal class MainWindowViewModel: ViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+
+        public string login;
+        public string password;
         public MainWindowViewModel()
         {
             #region Комманды
-           
+            AuthButtonClickCommand = new RelayCommand(OnAuthButtonClickCommandExecuted, CanAuthButtonClickCommandExecute);
             #endregion
         }
 
-        #region Заголовок окна
-        private string _Title = "Login Window";
+        private string _Title = "Login Page";
         public string Title
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
-        #endregion
 
-        #region Комманды
 
+        #region Команды
+        public ICommand? AuthButtonClickCommand { get; }
+        private void OnAuthButtonClickCommandExecuted(object p)
+        {
+            Login AuthUser = null;
+            using (SAYKOV_PDDContext db = new SAYKOV_PDDContext())
+            {
+                AuthUser = db.Logins.Where(b => b.LoginName == login && b.Passwords== password).FirstOrDefault();
+            }
+            if (AuthUser != null)
+                MessageBox.Show("All is good!");
+            else
+                MessageBox.Show("Проверьте введённые данные");
+        }
+
+        private bool CanAuthButtonClickCommandExecute(object p) => true;
         #endregion
     }
 }
